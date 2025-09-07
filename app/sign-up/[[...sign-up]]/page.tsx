94ee34b42d6920449623a813d828a6b5dd7deefb
YashChandan21 <yashparaschandan4@gmail.com>
2025-09-07 10:52:18 +0530

    Completed navbar, guest, about, contact pages

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className='flex items-center bg-white justify-center min-h-screen'>
+       <SignUp />
+    </div>
+ )
 }
